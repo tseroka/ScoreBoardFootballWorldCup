ff8b94207ab5a6fe6c4fb2a42445351efd4c29d0
Tymon Seroka <tymonseroka@gmail.com>
2022-07-03 20:16:26 +0200

    create project scaffold

---
+++ src/main/java/pl/tseroka/cup/world/football/board/score/HelloWorld.java
@@ -0,0 +1,7 @@
+package pl.tseroka.cup.world.football.board.score;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/groovy/pl/tseroka/cup/world/football/board/score/HelloWorldTests.groovy
@@ -0,0 +1,11 @@
+package pl.tseroka.cup.world.football.board.score
+
+import spock.lang.Specification
+
+class HelloWorldTests extends Specification {
+
+    def "test"() {
+        expect:
+        1 == 1
+    }
+}
